e1882d59cfc67b8842711e4386d1da83f16fe06b
riZ <50037152@europeia.pt>
2018-10-18 16:36:54 +0100

    First Commit
    Player Movement and Walking Animations

---
+++ Fire the Bullets/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+    private new Camera camera;
+
+	void Start () {
+        camera = GetComponent<Camera>();
+	}
+	
+	void Update () {
+        camera.orthographicSize = (Screen.height / 100f / 1.16988999999f);
+
+        if (target)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.position, 0.1f) + new Vector3(0, 0, -10f);
+        }
+    }
+}
