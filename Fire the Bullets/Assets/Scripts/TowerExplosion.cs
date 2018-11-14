cf423df2cfb0f5c18f75225c523b62639bb52d4c
riZ <50037152@europeia.pt>
2018-11-14 15:44:17 +0000

    Tower Bullets prefabs, tower scripts, tower ranges,
    changing camera size;
    Enemy spawner;
    Towers in general;

---
+++ Fire the Bullets/Assets/Scripts/TowerExplosion.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerExplosion : MonoBehaviour
+{
+
+    private Transform target;
+
+    public float speed = 70f;
+    public float explosionRadius = 0f;
+
+    public void Seek(Transform _target)
+    {
+        target = _target;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector2 direction = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+
+        if (direction.magnitude <= distanceThisFrame)
+        {
+            hitTarget();
+            return;
+        }
+
+        transform.Translate(direction.normalized * distanceThisFrame, Space.World);
+    }
+
+    void hitTarget()
+    {
+        if (explosionRadius > 0)
+        {
+            Explode();
+        }
+        else
+        {
+            Damage(target);
+        }
+
+        Destroy(gameObject);
+    }
+
+    void Explode(){
+        Collider2D[] colliders2D = Physics2D.OverlapCircleAll(transform.position, explosionRadius);
+        foreach (Collider2D collider in colliders2D)
+        {
+            if (collider.tag == "Enemy")
+            {
+                Damage(collider.transform);
+            }
+        }
+    }
+
+    void Damage(Transform enemy){
+        Destroy(enemy.gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
+    }
+}
---
+++ Fire the Bullets/Assets/Scripts/TowerHealer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerHealer : MonoBehaviour{
+
+    //public float timeBtwShots = 2f;
+
+    private Transform target;
+
+    public float speed = 70f;
+
+    public void Seek(Transform _target)
+    {
+        target = _target;
+    }
+
+    void Start()
+    {
+        //if (timeBtwShots <= 0)
+        //{
+        //    timeBtwShots -= Time.deltaTime;
+        //}
+    }
+
+    void Update()
+    {
+        if (target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector2 direction = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+
+        if (direction.magnitude <= distanceThisFrame)
+        {
+            hitTarget();
+            return;
+        }
+
+        transform.Translate(direction.normalized * distanceThisFrame, Space.World);
+    }
+
+    void hitTarget()
+    {
+        Destroy(gameObject);
+    }
+}
